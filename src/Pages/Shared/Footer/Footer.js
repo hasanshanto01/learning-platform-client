2b072b64295921b7e95c0805d71e5aaa7fbbaf7d
hasanshanto01 <hasanshanto01@gmail.com>
2022-10-25 23:34:09 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { FaCopyright, FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer</h2>
+        <div className='bg-primary text-center p-3 fixed-bottom'>
+            <p className='fs-5 fw-bold mb-0'>Contact us</p>
+            <div className='fs-3'>
+                <a href='https://www.facebook.com/' target='_blank'>
+                    <FaFacebook className='text-white' />
+                </a>
+                <a href='https://twitter.com/' target='_blank'>
+                    <FaTwitter className='ms-3 text-white' />
+                </a>
+                <a href='https://www.instagram.com/' target='_blank'>
+                    <FaInstagram className='ms-3 text-white' />
+                </a>
+                <a href='https://www.linkedin.com/' target='_blank'>
+                    <FaLinkedin className='ms-3 text-white' />
+                </a>
+            </div>
+            <p className='fs-5 mt-3'>All <FaCopyright className='text-white fs-6' />copyrights reserved to <span className='text-warning'>Skill</span> Hub</p>
         </div>
     );
 };
